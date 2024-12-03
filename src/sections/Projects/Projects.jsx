9a952cbd24db604dab6cd5180e932484536429a9
Om Rana <omrana8755800322@gmail.com>
2024-12-03 19:26:24 +0530

    first commit

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,29 @@
+import styles from './ProjectsStyles.module.css';
+import OminoS from '../../assets/OminoS.png';
+import Location from '../../assets/Location.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={StyleSheet.container}>
+        <h1 className="sectionTitle">Projects</h1> 
+        <div className={styles.projectsContainer}>
+          <ProjectCard 
+          src={OminoS} 
+          link="https://github.com/OmRana2004/Pizza-App"
+          h3="OminioS"
+          p="Pizza Divelery App"
+          />
+          <ProjectCard 
+          src={Location} 
+          link="https://github.com/OmRana2004/REALTIME_TRACKER"
+          h3="Location"
+          p="Real Time Location Tracker"
+          />
+            </div> 
+     </section>
+  );
+}
+
+export default Projects
+  
