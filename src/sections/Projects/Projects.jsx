c4554224e3b503122b67c0b4b9f29cdd2f10f2d0
OmRana2004 <omrana8755800322@gmail.com>
2025-06-01 22:30:56 +0530

    updating assets and projects section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,8 @@
 import styles from './ProjectsStyles.module.css';
-import Trekify from '../../assets/Location.png';
+import Trekify from '../../assets/Trekify.png';
 import OminoS from '../../assets/OminoS.png';
-import WeatherApp from '../../assets/Weather.png';
 import ProjectCard from '../../common/ProjectCard';
+import WeatherApp from '../../assets/Weather.png';
 
 
 
@@ -15,7 +15,7 @@ function Projects() {
           src={Trekify} 
           link="https://github.com/OmRana2004/Trekify"
           h3="Trekify"
-          p="Trekify is a Treking and Hiking App that helps you find the best trails and plan your next adventure."
+          p="Trekify "
           />
           <ProjectCard 
           src={OminoS} 
